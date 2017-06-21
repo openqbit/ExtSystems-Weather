d298be80af65cad27008e1f5a0c7420beae13114
Ashan <ashanijse@gmail.com>
2017-06-21 12:02:00 +0530

    class updated

---
+++ OpenQbit.WeatherSystem.git/OpenQbit.Weather.Test.DalTest/DalTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+using System.Linq;
+using OpenQbit.Weather.Common.Models;
+using OpenQbit.Weather.DAL.DataAccess;
+
+namespace OpenQbit.Weather.Test.DalTest
+{
+    [TestClass]
+    public class DalTest
+    {
+        private WeatherContext _db = new WeatherContext();
+        [TestMethod]
+        public void InsertCountryTest()
+        {
+            Country country = new Country { countryName = "TestCountry" };
+
+            _db.Country.Add(country);
+            _db.SaveChanges();
+
+            Country dbCountry = _db.Country.Where(c => c.countryName == "TestCountry").FirstOrDefault();
+
+            string actual, expected;
+
+            actual = dbCountry.countryName;
+            expected = "TestCountry";
+
+            Assert.AreEqual(expected, actual);
+            
+            _db.Country.Remove(dbCountry);
+
+        }
+
+        public void InsertDayDetailTest()
+        {
+
+        }
+
+        public void InsertForecastTest()
+        {
+
+        }
+
+        public void InsertForecastdetailTest()
+        {
+
+        }
+    }
+}
---
+++ OpenQbit.WeatherSystem.git/OpenQbit.Weather.WeatherService/CountryManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Weather.WeatherService.Contracts;
+using OpenQbit.Weather.DAL.DataAccess.Contracts;
+
+using Microsoft.Practices.Unity;
+
+namespace OpenQbit.Weather.WeatherService
+{
+    public class CountryManager : ICommonManager
+    {
+        private IRepository _repository;
+
+        [InjectionConstructor]
+        public CountryManager(IRepository repository)
+        {
+            //this._repository = repository;
+        }
+
+        public bool Recored<T>(T obj) where T : class
+        {
+            return _repository.Create(obj);
+        }
+
+        public bool Update<T>(T obj) where T : class
+        {
+            //return _repository.Update(obj);
+        }
+
+        public bool Delete<T>(T obj) where T : class
+        {
+            //return _repository.Delete(obj);
+        }
+
+        public List<T> GetAll<T>() where T : class
+        {
+            //return _repository.GetAll<T>();
+        }
+
+        public T Find<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            //return _repository.Find(predicate);
+        }
+
+        public List<T> FindList<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+           // return _repository.FindList(predicate);
+        }
+        
+        public bool Save()
+        {
+            //return _repository.Save();
+        }
+
+       
+    }
+}
