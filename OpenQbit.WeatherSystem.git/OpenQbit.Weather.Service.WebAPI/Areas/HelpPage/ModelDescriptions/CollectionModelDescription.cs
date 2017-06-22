6794437cc46809b3789d921071d10c9d2ccf4191
sachinlakshitha <hks.lakshitha@gmail.com>
2017-06-22 14:43:35 +0530

    presentation layer bug fixed

---
+++ OpenQbit.WeatherSystem.git/OpenQbit.Weather.Service.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace OpenQbit.Weather.Service.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ OpenQbit.WeatherSystem.git/OpenQbit.Weather.Service.WebAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace OpenQbit.Weather.Service.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
