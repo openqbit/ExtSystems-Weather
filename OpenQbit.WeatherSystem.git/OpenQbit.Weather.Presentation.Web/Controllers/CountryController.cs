7b045ad147d5f72af70154e9fec5b0be35734be9
Ashan <ashanijse@gmail.com>
2017-07-05 12:03:05 +0530

    Creating CountryController And Day DetailController in presentation.web

---
+++ OpenQbit.WeatherSystem.git/OpenQbit.Weather.Presentation.Web/Controllers/CountryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OpenQbit.Weather.Presentation.Web.Models;
+
+namespace OpenQbit.Weather.Presentation.Web.Controllers
+{
+    public class CountryController :Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            WebClient client = new WebClient();
+            Customer customer = await client.Get();
+
+            return View(country);
+        }
+    }
+}
