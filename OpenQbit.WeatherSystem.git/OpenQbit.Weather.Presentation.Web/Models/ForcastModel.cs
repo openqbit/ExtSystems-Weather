2b94810866c9cbb9e5dc1714fa401938b5261293
Ashan <ashanijse@gmail.com>
2017-07-05 11:46:18 +0530

    Creating Forcast Model in presentation.web

---
+++ OpenQbit.WeatherSystem.git/OpenQbit.Weather.Presentation.Web/Models/ForcastModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQbit.Weather.Presentation.Web.Models
+{
+    public class ForcastModel
+    {
+        public int Id { get; set; }
+        
+        public int CountryId { get; set; }
+        public int DaydetailId { get; set; }
+        public DateTime Date { get; set; }
+        
+        public string Status { get; set; }
+        
+    }
+}
