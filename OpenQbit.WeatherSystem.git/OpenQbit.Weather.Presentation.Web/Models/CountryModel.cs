ccc56951d5ea0b3e4ef719673b938d76c99ba0b7
Ashan <ashanijse@gmail.com>
2017-07-05 11:16:39 +0530

    Creating day Detail Model And Country Model

---
+++ OpenQbit.WeatherSystem.git/OpenQbit.Weather.Presentation.Web/Models/CountryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQbit.Weather.Presentation.Web.Models
+{
+    public class CountryModel
+    {
+        public int Id { get; set; }
+        public string countryName { get; set; }
+
+       /* public static implicit operator CountryModel( v)
+        {
+            throw new NotImplementedException();
+
+        }*/
+}
