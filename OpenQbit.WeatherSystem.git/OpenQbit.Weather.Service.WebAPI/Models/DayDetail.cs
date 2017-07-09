90f30fc10194280909e87ee20a65340350183496
Ashan <ashanijse@gmail.com>
2017-07-09 21:50:22 +0530

    Creating DayDetails Model In Wep API in Service Layer

---
+++ OpenQbit.WeatherSystem.git/OpenQbit.Weather.Service.WebAPI/Models/DayDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQbit.Weather.Service.WebAPI.Models
+{
+    public class DayDetail
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string DayDescription { get; set; }
+        public string NightDescription { get; set; }
+        public DateTime Sunrise { get; set; }
+        public DateTime Sunset { get; set; }
+        public DateTime Moonrise { get; set; }
+        public DateTime Moonset { get; set; }
+    }
+}
