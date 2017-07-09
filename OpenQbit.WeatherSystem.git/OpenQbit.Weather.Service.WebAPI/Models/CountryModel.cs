a5a79947de5363d103c40d7bba226c361ae8b238
Ashan <ashanijse@gmail.com>
2017-07-09 22:00:09 +0530

    Creating Country Model In Wep API in Service Layer

---
+++ OpenQbit.WeatherSystem.git/OpenQbit.Weather.Service.WebAPI/Models/CountryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQbit.Weather.Service.WebAPI.Models
+{
+    public class CountryModel
+    {
+        public int Id { get; set; }
+        public string countryName { get; set; }
+    }
+}
