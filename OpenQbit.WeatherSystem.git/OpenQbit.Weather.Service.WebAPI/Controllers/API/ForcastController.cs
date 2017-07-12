d94afe1ac9d5a6e050bfbac8da3289deac4275ce
Ashan <ashanijse@gmail.com>
2017-07-12 10:34:24 +0530

    Creating ForcastModel in Wb API Service Layer

---
+++ OpenQbit.WeatherSystem.git/OpenQbit.Weather.Service.WebAPI/Controllers/API/ForcastController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using OpenQbit.Weather.Service.WebAPI.Models;
+
+namespace OpenQbit.Weather.Service.WebAPI.Controllers.API
+{
+    public class ForcastController : ApiController
+    {
+        public ForcastModel Get(int id)
+        {
+            ForcastModel forcast = new ForcastModel
+            {
+                Id = 1,
+                CountryId = 1,
+                DaydetailId = 1,
+                Date = new DateTime(12 - 07 - 2017),
+                Status = "Cloudy"
+            };
+            return forcast;
+        }
+        public List<ForcastModel> getList()
+        {
+            List<ForcastModel> ForcastList = new List<ForcastModel>();
+
+            ForcastModel forcast1 = new ForcastModel
+            {
+                Id = 1,
+                CountryId = 1,
+                DaydetailId = 1,
+                Date = new DateTime(12 - 07 - 2017),
+                Status = "Cloudy"
+            };
+
+            ForcastModel forcast2 = new ForcastModel()
+            {
+                Id = 1,
+                CountryId = 1,
+                DaydetailId = 1,
+                Date = new DateTime(12 - 07 - 2017),
+                Status = "Cloudy"
+            };
+            ForcastList.Add(forcast1);
+            ForcastList.Add(forcast2);
+
+            return ForcastList;
+        }
+    }
+}
+
